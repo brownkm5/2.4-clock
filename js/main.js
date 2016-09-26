d90cb1479368b9324f537442fe42ba0b773078ac
Kevin Brown <brownkm5@g.cofc.edu>
2016-09-26 14:30:09 -0400

    fixed styling and hover effect

--- js/main.js
+++ js/main.js
@@ -1,43 +1,65 @@
 (function(){
 "use strict";
+var clock = document.getElementById('clock');
+var background = document.getElementById('background');
+var hoursContainer = document.getElementById("hours")
+var minutesContainer = document.getElementById("minutes")
+var secondsContainer = document.getElementById("seconds")
+
+var isHovering=false;
 
 function logCurrentTime(){
 var day = new Date();
 var hours = day.getHours ();
-if (hours>12) {
-  hours=hours-12
-}
+// if (hours>12) {
+//   hours=hours-12
+// }
 var minutes = day.getMinutes ();
 var seconds = day.getSeconds ();
 
-var body = document.getElementById(body);
+var currentRed = ('0' + hours.toString(16)).slice(-2);
+var currentGreen = ('0' + minutes.toString(16)).slice(-2);
+var currentBlue = ('0' + seconds.toString(16)).slice(-2);
+var colorCode= '#' + currentRed + currentGreen + currentBlue;
+// var newHex = '#' + h + m + s;
+// previousHex = newHex
+// console.log(newHex);
 
-var h = document.getElementById("hours").textContent = ("0" + hours).slice(-2);
-var m = document.getElementById("minutes").textContent = ("0" + minutes).slice(-2);
-var s = document.getElementById("seconds").textContent = ("0" + seconds).slice(-2);
+//set the background based on hours minutes seconds
+background.style.backgroundColor = colorCode;
+//update the clock display
+if (isHovering){
+  hoursContainer.textContent = currentRed;
+  minutesContainer.textContent =currentGreen;
+  secondsContainer.textContent =currentBlue;
+}
+else {
+hoursContainer.textContent = ('0' + hours).slice(-2);
+minutesContainer.textContent = ('0' + minutes).slice(-2);
+secondsContainer.textContent = ('0' + seconds).slice(-2);
+}
+// var h = document.getElementById("hours").textContent = ("0" + hours).slice(-2);
+// var m = document.getElementById("minutes").textContent = ("0" + minutes).slice(-2);
+// var s = document.getElementById("seconds").textContent = ("0" + seconds).slice(-2);
 
 var secondbar = (seconds/60)*45;
 document.querySelector('.secondbar').style.width = secondbar.toString() + '%';
 
-var newHex = '#' + h + m + s;
-previousHex = newHex
 // i looked up how to add a radial-gradient but i couldnt get it to work, i think the syntax is wrong
 // var radialValue = 'radial-gradient(' circle' + ',farthest-corner,' + 'previousHex,' + 'newHex ')';
 // body.style.background = radialValue;
 }
-window.setInterval(logCurrentTime, 1000);
-var previousHex = ' ';
+window.setInterval(logCurrentTime, 100);
+// var previousHex = ' ';
 
-// i tried to make the hover work with this but i think my syntax is wrong with this also. im looking on mdn and stackoverflow but i couldnt find anything to fix it
-// function initHide(){
-//   var clock= document.getElementById('clock');
-//   clock.addEventListener('mouseover', function(event)){
-//     event.target.style.display = 'none';
-//   }
-// }
-
-
-
+function mouseOverHandler(){
+  isHovering = true;
+}
+function mouseOutHandler(){
+  isHovering = false;
+}
+clock.addEventListener('mouseover', mouseOverHandler);
+clock.addEventListener('mouseout', mouseOutHandler);
 
 
 }());
